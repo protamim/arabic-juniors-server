be44bcd931267a79f8bd1214df67b4a282ffcc39
protamim <s163.tamim@gmail.com>
2025-06-14 23:15:06 +0600

    refactor: replace getUserLocation with getClientLocation for improved location handling

--- api/routes/homeRoutes.ts
+++ api/routes/homeRoutes.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from "express";
+import { getClientLocation } from "../utils/getClientLocation";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  console.log('Get / hit')
+router.get("/", async (req: Request, res: Response) => {
+  const userInfo = await getClientLocation(req);
   res.send({
     message: `You're landed in an empty ocean! 😊`,
     today: `${Date.now()}`,
+    info: {
+      city: userInfo.city,
+      country: userInfo.country,
+      region: userInfo.region,
+      success: userInfo.success,
+    },
   });
 });
 
--- api/utils/getUserLocation.ts
+++
@@ -1,5 +0,0 @@
-export async function getUserLocation() {
-  const res = await fetch("https://ipwho.is");
-  const data = await res.json();
-  return data;
-}
